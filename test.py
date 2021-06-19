65d3576a9130ffb99c457df30297aa00c13bdc26
Ajay <ajay.23432@gmail.com>
2021-06-19 15:37:32 +0530

    added test regex

--- test.py
+++ test.py
@@ -1,3 +1,49 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango","apple","kiwi"]
-fruits.insert(7,"apple")
-print(fruits)
+
+import re
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+Ha HaHa
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+coreyms.com
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+
+
+'''
+emails = '''
+CoreyMSchafer@gmail.com
+corey.schafer@university.edu
+corey-321-schafer@my-work.net
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+pattern = re.compile(r'[a-zA-Z-0-9.]+@[a-zA-Z-]+\.(com|edu|net)')
+
+matches = pattern.finditer(emails)
+
+for match in matches:
+    print(match)
+
+# with open('data.txt', 'r') as f:
+#     contents = f.read()
+#     matches = pattern.finditer(contents)
+#
+#     for match in matches:
+#         print(match)
+
+
+
+
